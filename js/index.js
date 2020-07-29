c0bc024d0ce0708031472cc6158e6b81f6a04a43
סיון הרט <sivan13h@gmail.com>
2020-07-29 15:32:33 +0200

    https

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get("http://www.omdbapi.com/", {
+    const response = await axios.get("https://www.omdbapi.com/", {
       params: {
         apikey: "61df7f9d",
         s: searchTerm,
@@ -46,7 +46,7 @@ let leftMovie;
 let rightMovie;
 
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: {
       apikey: "61df7f9d",
       i: movie.imdbID,
